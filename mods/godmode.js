160cfece422276d3ee3cfbdced28d554ade9349e
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-02-20 09:32:38 -0800

    Update godmode.js

--- mods/godmode.js
+++ mods/godmode.js
@@ -2,6 +2,8 @@
 NOTE: This will only work in singleplayer, wich does not exist anymore
 */
 PluginAPI = ModAPI;
+
+PluginAPI.displayToChat({msg: "NOTE: Godmode will only work in singleplayer."})
 PluginAPI.require("player");
 
 var isToggled = false; //stores whether the plugin is on or off
